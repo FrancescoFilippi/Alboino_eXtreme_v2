f54413055fa0a2a5100657b59ee9ca23002514d7
Francesco <misterp.89@hotmail.it>
2017-12-14 14:21:30 +0100

    Aggiunto hp-scan da home page
    Aggiunto hp-scan da home page

--- client/main.js
+++ client/main.js
@@ -1,14 +1,24 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
 
 import './main.html';
 
-Template.body.onRendered(function(){
-  
+// Template body generale. Va sistemato tutto in sotto-template.
+Template.body.events({
+  // click sul bottone #scanner, lancia il metodo server 'runCode' che runna il comando shell
+  // Aggiungere fileName e estensione per il file. Va fatto in un template a parte.
+  'click #scanner': function () {
+    Meteor.call('runCode', function (err, response) {
+      console.log(response);
+    });
+  }
+});
+
+Template.body.onRendered(function(){ 
   // Home page menu
   $(document)
     .ready(function() {
-
       // fix menu when passed
       $('.masthead')
         .visibility({
@@ -21,12 +31,10 @@ Template.body.onRendered(function(){
           }
         })
       ;
-
       // create sidebar and attach to menu open
       $('.ui.sidebar')
         .sidebar('attach events', '.toc.item')
       ;
-
     })
   ;
 })
--- server/main.js
+++ server/main.js
@@ -1,5 +1,35 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+// Meteor.startup(() => {
+//   // code to run on server at startup
+// });
+
+Meteor.startup(function(){
+  // carica future
+  var Future = Npm.require("fibers/future");
+  // carica exec
+  var exec = Npm.require("child_process").exec;
+ 
+  // Metodo per il server
+  // Aggiungere variabili per la scelta del nome del file e dell'estensione
+  Meteor.methods({
+    runCode: function () {
+      // La chiamata aspetta asincrona che finisca l'esecuzione; 'unblock' permette
+      // di mettere in coda le chiamate
+      this.unblock();
+      var future=new Future();
+      // Comando Shell per lanciare la scansione.
+      // Il file per ora viene salvato nella cartella /public nella cartella del progetto, da sistemare per servire i file al browser
+      // Sarà possibile scegliere l'estensione del file. 
+      var command='hp-scan -o /home/aulettarappresentanti/meteor/Alboino_eXtreme_v2/public/';
+      exec(command,function(error,stdout,stderr){
+        if(error){
+          console.log(error);
+          throw new Meteor.Error(500,command+" failed");
+        }
+        future.return(stdout.toString());
+      });
+      return future.wait();
+    }
+  });
 });
