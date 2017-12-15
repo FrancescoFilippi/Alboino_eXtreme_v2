b5f053c4088df63eb50b777843c66270f4b061e5
Francesco <misterp.89@hotmail.it>
2017-12-15 17:14:14 +0100

    Test invio mail con file scannerizato
    Test invio mail con file scannerizato

--- client/main.js
+++ client/main.js
@@ -30,22 +30,32 @@ Template.body.onRendered(function(){
 })
 
 // Template Scanner.
+Template.scanner.onRendered(function(){
+  $(function(){
+    $("#test").click(function(){
+      $(".test").modal('show');
+    });
+    $(".test").modal({
+      closable: true
+    });
+  });
+});
 Template.scanner.events({
   // click sul bottone #scanner, lancia il metodo server 'runCode' che runna il comando shell
   // Aggiungere estensione per il file.
   'click #scannerButton': function (event, template) {
     event.preventDefault();
-    var fileName;
-
-    if(template.find('#inputFileName').value){
-      fileName = template.find('#inputFileName').value;
-    }
-    if(!template.find('#inputFileName').value){
-      fileName = 'hpscan' + date.valueOf();
-    } 
+    var date = new Date();
+    var fileName = (event.target.inputNomeFile.value) ? event.target.inputNomeFile.value : 'hpscan-' + date.valueOf();
+    var emailAddress = event.target.inputEmailAddress.value;
+    var attachments = {
+      fileName: fileName,
+      filePath: '/home/aulettarappresentanti/meteor/Alboino_eXtreme_v2/public/scanner' + fileName + '.png'
+    };
     
     Meteor.call('runCode', fileName, function (err, response) {
       console.log(response);
+      Meteor.call('sendMail', emailAddress, "postmaster@sandboxe3362d49940a40608beb65efd5554f84.mailgun.org", "HP-SCAN: " + fileName, attachments);
     });
   }
 });
