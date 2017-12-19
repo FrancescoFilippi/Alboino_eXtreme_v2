c10ec655df5576398eff9b39b1d97fa379989492
Francesco <misterp.89@hotmail.it>
2017-12-19 11:39:20 +0100

    chk mail

--- client/main.js
+++ client/main.js
@@ -35,24 +35,30 @@ Template.scanner.events({
   'click #openScannerAndEmailButton': function(event, template){
     event.preventDefault();
     $(function(){
+      $('#ScanningAndSendingProgressBar').progress();
       $(".scannerAndMailModal").modal('show');
+      incrementProgressBar('#ScanningAndSendingProgressBar');
       $(".scannerAndMailModal").modal({
         closable: true,
         onApprove: function(){
           var date = new Date();
           var fileName = ($('input[name="inputNomeFile"]').val()) ? ($('input[name="inputNomeFile"]').val()) : 'hpscan-' + date.valueOf();
           var emailAddress = ($('input[name="inputEmailAddress"]').val());
+          var isSendingMail = $('#mailConfirm').attr("checked", true);
           var attachments = {
             filename: fileName + '.png',
             path: '/home/aulettarappresentanti/meteor/Alboino_eXtreme_v2/public/scanner/' + fileName + '.png',
             //contentType: 'png'
           };
+          // incrementProgressBar('#ScanningAndSendingProgressBar');
           Meteor.call('hpscan', fileName, function (err, response) {
             console.log(response);
-            Meteor.call('sendMail', emailAddress, "postmaster@sandboxe3362d49940a40608beb65efd5554f84.mailgun.org", "HP-SCAN: " + fileName, "No ma, brava Fede!" ,attachments, 
-            function(err, response){
-              console.log(response);
-            });
+            if(isSendingMail){
+              Meteor.call('sendMail', emailAddress, "postmaster@sandboxe3362d49940a40608beb65efd5554f84.mailgun.org", "HP-SCAN: " + fileName, "No ma, brava Fede!" ,attachments, 
+              function(err, response){
+                console.log(response);
+              });
+            }
           });
           console.log(fileName, emailAddress, attachments);
         }
@@ -60,3 +66,14 @@ Template.scanner.events({
     });
   }
 });
+
+//$("#chart").attr("data-percent", value.toString());
+function incrementProgressBar(elem){
+  var secs = 1000 * 60;
+  var count = 0;
+  setInterval(function() {
+      var $progressBar = $(elem);
+      count += 10;
+      $progressBar.attr("data-percent", count.toString());
+  }, secs);
+}
